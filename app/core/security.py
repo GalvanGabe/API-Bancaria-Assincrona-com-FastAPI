24968c2a8453fdd86f14d6419593c5d86941f5c4
Gabriel Galvan <galvan.gabriel@hotmail.com>
2026-05-19 22:42:55 -0300

    auth routes

---
+++ app/core/security.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import select
+
+from app.database.dependencies import get_db
+from app.models.user import User
+
+SECRET_KEY = "chave-secreta"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
+    )
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciais inválidas.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email: str | None = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+        
+    except JWTError:
+        raise credentials_exception
+    
+    query = select(User).where(User.email == email)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise credentials_exception
+    
+    return user
--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.routes import auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
